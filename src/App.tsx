12e56afda0d0ca82376c47dbf0d3d8d2909af6c7
Adrian Knapp <adrknapp@gmail.com>
2021-09-29 09:36:57 -0300

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 
 import { TransactionsProvider } from './hooks/useTransactions';
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
+
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('#root');
